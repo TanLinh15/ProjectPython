90f763b3f19bf125bff93746395e0c60d1d18ac2
Phi Long <paviliondd@gmail.com>
2021-10-29 22:36:47 +0700

    update module profile

--- app.py
+++ app.py
@@ -109,3 +109,19 @@ def logout():
    session.pop('username', None)
    # Redirect to login page
    return redirect(url_for('login'))
+
+   @app.route('/profile')
+def profile(): 
+ # Check if account exists using MySQL
+    conn = mysql.connect()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+  
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', [session['id']])
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
